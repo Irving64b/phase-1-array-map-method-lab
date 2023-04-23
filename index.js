4d6283beccdccdc69fc49b5416efee45fa9b876d
Irving Medina <irving.64@hotmail.com>
2023-04-22 23:33:42 -0400

    complete map

--- index.js
+++ index.js
@@ -12,5 +12,11 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+//  return tutorials
+//}
+let name = tutorials.map((tutorial) => {
+tutorial.split(' ')
+return tutorial.split(' ').map((word) => word[0].toUpperCase() + word.slice(1)).join(' ');
+});
+return name
 }
